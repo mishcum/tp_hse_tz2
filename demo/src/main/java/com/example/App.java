0dafe0b2a2a0fac848dc27612cda11eb80e56c81
Mikhail <m6453327@icloud.com>
2024-05-18 13:26:13 +0300

    upd 18

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -65,7 +65,35 @@ public class App {
     
 
     public static void main(String[] args) throws FileNotFoundException {
-        
+        App T = new App("demo/src/test/java/com/example/test1.txt");
+        App T2 = new App("demo/src/test/java/com/example/test2.txt");
+        App T3 = new App("demo/src/test/java/com/example/test3.txt");
+        App T4 = new App("demo/src/test/java/com/example/test4.txt");
+        App T5 = new App("demo/src/test/java/com/example/test5.txt");
+        System.out.println("Min: " + T._min());
+        System.out.println("Max: " + T._max());
+        System.out.println("Sum: " + T._sum());
+        System.out.println("Mult: " + T._mult());
+
+        System.out.println("Min: " + T2._min());
+        System.out.println("Max: " + T2._max());
+        System.out.println("Sum: " + T2._sum());
+        System.out.println("Mult: " + T2._mult());
+
+        System.out.println("Min: " + T3._min());
+        System.out.println("Max: " + T3._max());
+        System.out.println("Sum: " + T3._sum());
+        System.out.println("Mult: " + T3._mult());
+
+        System.out.println("Min: " + T4._min());
+        System.out.println("Max: " + T4._max());
+        System.out.println("Sum: " + T4._sum());
+        System.out.println("Mult: " + T4._mult());
+
+        System.out.println("Min: " + T5._min());
+        System.out.println("Max: " + T5._max());
+        System.out.println("Sum: " + T5._sum());
+        System.out.println("Mult: " + T5._mult());
     }
 }
 
